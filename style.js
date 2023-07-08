2166227a1a4a15f9ad81d4ace919c438745b04f4
CajuiNotification <138515098+CajuiNotification@users.noreply.github.com>
2023-07-08 20:41:26 -0300

    first commit

--- style.js
+++ style.js
@@ -30,60 +30,7 @@ $(document).ready(function () {
     const mobileMenu = new MobileNavbar(".mobile-menu", ".side-bar");
     mobileMenu.addClickEvent();
 
-    firebase.auth().onAuthStateChanged(function (user) {
-
-        db.collection("usuarios(Site)").where("uid", "==", "YkBuDKlulNeII9ug87e55zO2jSH2").get()
-            .then((docRef) => {
-
-                docRef.forEach(doc => {
-
-                    const nomeUser = document.querySelector('div[class="nome"]');
-
-                    uidSession = user.uid;
-                    nomeSession = doc.data().nome;
-                    emailSession = doc.data().email;
-                    senhaSession = doc.data().senha;
-
-                    const emailUser = document.querySelector('div[class="email"]');
-
-                    const email = doc.data().email;
-
-                    firebase.storage().ref().child(nomeSession).getDownloadURL()
-                        .then((url) => {
-
-                            const img = document.getElementById('imgPhoto');
-                            img.setAttribute('src', url);
-                        })
-                        .catch((error) => {
-
-                            if (error.code == "storage/object-not-found") {
-
-                                firebase.storage().ref().child("vazio").child("personIcon.jpg").getDownloadURL()
-                                    .then((url) => {
-
-                                        const img = document.getElementById('imgPhoto');
-                                        img.setAttribute('src', url);
-                                    })
-                                    .catch((error) => {
-                                        alert(error.message);
-                                    });
-
-                            } else {
-
-                                alert(error.message);
-
-                            }
-                        });
-
-                    nomeUser.innerHTML = nomeSession;
-                    emailUser.innerHTML = email;
-
-                });
-            })
-            .catch((error) => {
-                console.log(error.message);
-            })
-    })
+    
 
     document.querySelector("a[name='logout']").addEventListener("click", function () {
         firebase.auth().signOut()
